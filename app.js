a9496a66c3b0ce783a5e3c599b3cfea1dac8d32a
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-08-14 15:11:53 +0100

    Initil commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// Define UI vars
+const form = document.querySelector('#task-form'); // or get elementById...
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// load all event listeners
